9da2c4dabd4b353ab2feb1a8d3b2b957874851a0
Rohit <rohit.rustagi@daffodilsw.com>
2019-02-24 15:24:44 +0530

    Register user in beginning in context temp

--- src/app/lib/fulfillment.js
+++ src/app/lib/fulfillment.js
@@ -14,10 +14,12 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
   (request, response) => {
     const agent = new WebhookClient({ request, response });
 
+    const maxDocListCount = 2;
+
     const somesuggests = [
-      "Hi",
+      "Search Gyno",
+      "Search Skin Doctor",
       "Search Doctors",
-      "Search",
       "Search Physio",
       "Look for a Dentist",
       "Search Physician",
@@ -28,9 +30,9 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
       "Search Physiotherapist"
     ];
 
-    console.log(
-      "Dialogflow Request headers: " + JSON.stringify(request.headers)
-    );
+    // console.log(
+    //   "Dialogflow Request headers: " + JSON.stringify(request.headers)
+    // );
     console.log("Dialogflow Request body: " + JSON.stringify(request.body));
 
     function helpIntent(agent) {
@@ -40,6 +42,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
       agent.add(new Suggestion(somesuggests[randomNumber()]));
       agent.add(new Suggestion(somesuggests[randomNumber()]));
       agent.add(new Suggestion(somesuggests[randomNumber()]));
+      updateUserContext(agent);
     }
 
     function welcome(agent) {
@@ -49,6 +52,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
       agent.add(new Suggestion(somesuggests[randomNumber()]));
       agent.add(new Suggestion(somesuggests[randomNumber()]));
       agent.add(new Suggestion(somesuggests[randomNumber()]));
+      updateUserContext(agent);
     }
 
     function fallback(agent) {
@@ -58,6 +62,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
       agent.add(new Suggestion(somesuggests[randomNumber()]));
       agent.add(new Suggestion(somesuggests[randomNumber()]));
       agent.add(new Suggestion(somesuggests[randomNumber()]));
+      updateUserContext(agent);
     }
 
     function AskDoctor(agent) {
@@ -67,24 +72,50 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
         agent.parameters.SQH_DoctorField.indexOf("Doctor") < 0
       ) {
         const collection = "doctors";
+
         const dialogflowAgentDoc = db
           .collection(collection)
-          .where("field", "==", agent.parameters.SQH_DoctorField);
+          .where("field", "==", agent.parameters.SQH_DoctorField)
+          .orderBy("index");
+        //.limit(maxDocListCount);
+
         // Get the value of 'entry' in the document and send it to the user
         return dialogflowAgentDoc
           .get()
           .then(docs => {
+            console.log("Size" + docs.size);
             if (docs.size > 0) {
               agent.add(`Here are some ${agent.parameters.SQH_DoctorField}`);
+              let currentCount = 0;
               docs.forEach(function(doc) {
-                console.log(doc.id);
-                const data = doc.data();
-
-                agent.add(data.index + " - " + data.name);
-
-                agent.add(new Suggestion(data.index.toString()));
+                if (currentCount < maxDocListCount) {
+                  currentCount++;
+                  console.log(doc.id);
+                  const data = doc.data();
+                  console.log(data);
+                  agent.add(data.index + " - " + data.name);
+                  agent.add(new Suggestion(data.index.toString()));
+                }
               });
-              agent.add("Reply with the number against the doctor.");
+              if (docs.size > maxDocListCount) {
+                agent.add(
+                  `We have more ${docs.size - currentCount}  ${
+                    agent.parameters.SQH_DoctorField
+                  }. Say "more".`
+                );
+                agent.add(new Suggestion("more"));
+              }
+
+              const context = {
+                name: "lastdocid",
+                lifespan: 5,
+                parameters: { max: maxDocListCount }
+              };
+              agent.setContext(context);
+
+              agent.add(
+                "To proceed, reply with the number against the doctor."
+              );
               agent.add(new Suggestion(`Cancel`));
             } else {
               agent.add(
@@ -105,6 +136,69 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
         agent.add(new Suggestion(somesuggests[randomNumber()]));
         agent.add(new Suggestion(somesuggests[randomNumber()]));
       }
+    }
+
+    function RequestMoreDoctor(agent) {
+      console.log("context search");
+
+      let lastContext = agent.getContext("lastdocid");
+      let docCount = lastContext.parameters.max;
+      let docField = lastContext.parameters.SQH_DoctorField;
+
+      console.log("count", docCount);
+      console.log("search", docField);
+
+      const collection = "doctors";
+
+      const dialogflowAgentDoc = db
+        .collection(collection)
+        .where("field", "==", docField)
+        .offset(docCount);
+
+      //.limit(maxDocListCount);
+      updateUserContext(agent);
+      // Get the value of 'entry' in the document and send it to the user
+      return dialogflowAgentDoc
+        .get()
+        .then(docs => {
+          console.log("Size" + docs.size);
+          if (docs.size > 0) {
+            let currentCount = 0;
+            docs.forEach(function(doc) {
+              if (currentCount < maxDocListCount) {
+                currentCount++;
+                console.log(doc.id);
+                const data = doc.data();
+                console.log(data);
+                agent.add(data.index + " - " + data.name);
+                agent.add(new Suggestion(data.index.toString()));
+              }
+            });
+            if (docs.size > maxDocListCount) {
+              agent.add(
+                `We have more ${docs.size - docCount} ${docField}. Say "more"`
+              );
+              agent.add(new Suggestion("more"));
+            }
+            //updating context for next run
+
+            let countGoing = docCount + maxDocListCount;
+            let count_doc_Context = {
+              name: "lastdocid",
+              lifespan: 5,
+              parameters: { max: countGoing }
+            };
+            agent.setContext(count_doc_Context);
+
+            agent.add("To proceed, reply with the number against the doctor.");
+            agent.add(new Suggestion(`Cancel`));
+          } else {
+            agent.add(`Sorry, currently no ${docField} are available.`);
+          }
+        })
+        .catch(err => {
+          console.log("Error while fetching data from: " + collection + err);
+        });
     }
 
     function finalizeDoctor(agent) {
@@ -128,6 +222,13 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
                     data.name +
                     ". Would you like to proceed with it ?"
                 );
+                let selectedDoctor = data.name;
+                let doc_Context = {
+                  name: "doc_context",
+                  lifespan: 3,
+                  parameters: { docName: selectedDoctor }
+                };
+                agent.setContext(doc_Context);
               });
               agent.add(new Suggestion(`Yes`));
               agent.add(new Suggestion(`No`));
@@ -154,8 +255,14 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
       const time = agent.parameters.time;
       console.log("contexts" + JSON.stringify(agent.contexts));
       console.log("parameters" + JSON.stringify(agent.parameters));
+      //Get Doctor context
+      let docContext = agent.getContext("doc_context");
+      let docName = docContext.parameters.docName;
+      //Get User context
+      let userContext = agent.getContext("user_context");
+      let userName = userContext.parameters.user;
 
-      const entry = { date, time };
+      const entry = { date, time, docName, userName };
 
       // Get the database collection 'dialogflow' and document 'agent' and store
       // the document  {entry: "<value of database entry>"} in the 'agent' document
@@ -179,8 +286,19 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
     }
 
     function checkAppointments(agent) {
+      console.log("contexts" + JSON.stringify(agent.contexts));
+
+      //Get User context
+      let userContext = agent.getContext("user_context");
+      let userName = userContext.parameters.user;
+
       const appointments = "appointments";
-      const dialogflowAgentDoc = db.collection(appointments);
+      // const dialogflowAgentDoc = db.collection(appointments);
+
+      const dialogflowAgentDoc = db
+        .collection(appointments)
+        .where("userName", "==", userName);
+
       // Get the value of 'entry' in the document and send it to the user
       return dialogflowAgentDoc
         .get()
@@ -193,7 +311,9 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
                 "Date: " +
                   new Date(data.date).toDateString() +
                   " - Time: " +
-                  data.time
+                  data.time +
+                  " with " +
+                  data.docName
               );
             });
             agent.add(new Suggestion(`Thanks`));
@@ -239,11 +359,45 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
       return Math.ceil(Math.random() * 10);
     }
 
+    function RegisterUser(agent) {
+      // Get parameter from Dialogflow with the string to add to the database
+      const query = agent.query.split(" ");
+      const user = query.slice(1).join(" ");
+
+      let user_Context = {
+        name: "user_context",
+        lifespan: 10,
+        parameters: { user: user }
+      };
+
+      agent.setContext(user_Context);
+    }
+
+    function updateUserContext(agent) {
+      let userContext = agent.getContext("user_context");
+
+      let user_Context = {
+        name: "user_context",
+        lifespan: 10,
+        parameters: { user: userContext.parameters.user }
+      };
+
+      agent.setContext(user_Context);
+    }
+
+    function SayTime() {
+      var d = new Date();
+      agent.add(`The time is ${d.toDateString()}`);
+    }
+
     // Run the proper function handler based on the matched Dialogflow intent name
     const intentMap = new Map();
+    intentMap.set("Ask Time", SayTime);
+    intentMap.set("Register User", RegisterUser);
     intentMap.set("Default Welcome Intent", welcome);
     intentMap.set("Default Fallback Intent", fallback);
     intentMap.set("Ask Doctor Intent", AskDoctor);
+    intentMap.set("Ask Doctor Intent - next", RequestMoreDoctor);
     intentMap.set("Help Intent", helpIntent);
     intentMap.set(
       "Select Doctor Intent - yes - datetime",
